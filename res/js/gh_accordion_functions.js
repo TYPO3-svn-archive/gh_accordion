ca05afe98aff0d4ebac368bbf22509d06b333ed2
ghermens <ghermens@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-04-03 08:36:33 +0000

    Effect is now configurable via Constant Editor
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/gh_accordion/trunk@31883 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/js/gh_accordion_functions.js
+++ res/js/gh_accordion_functions.js
@@ -1,5 +1,8 @@
+var txghaccordioneffect = 'blind';
+var txghaccordionoptions = { duration: 0.3 };
+
 function txghaccordionsingletoggle(uid) {
-	Effect.toggle('tx_ghaccordion_content_' + uid, 'blind', { duration: 0.5 });
+	Effect.toggle('tx_ghaccordion_content_' + uid, txghaccordioneffect, txghaccordionoptions );
 	if( $('tx_ghaccordion_header_' + uid).hasClassName('tx_ghaccordion_status0')) {
 		$('tx_ghaccordion_header_' + uid).removeClassName('tx_ghaccordion_status0');
 		$('tx_ghaccordion_header_' + uid).addClassName('tx_ghaccordion_status1');
